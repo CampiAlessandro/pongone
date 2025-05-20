12020c6a05064a7de8dc0f011f3e1a2415b01195
Alessandro Campi <campialessandrodev@gmail.com>
2025-05-20 11:37:54 +0200

    Refactor ball.gd for code consistency by adding spaces around colons and operators.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
-@export var speed:float = 10.0
-const FAKE_CURVE_FACTOR:float = 0.005
+@export var speed: float = 10.0
+const FAKE_CURVE_FACTOR: float = 0.005
 @onready var hit_sound: AudioStreamPlayer2D = $HitSoundPLayer
 
 var direction: Vector2
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 	var object_hit = move_and_collide(speed * direction)
 	handle_collision(object_hit)
 
-func handle_collision(object_hit:KinematicCollision2D):
+func handle_collision(object_hit: KinematicCollision2D):
 	if !object_hit:
 		return
 	
@@ -22,20 +22,19 @@ func handle_collision(object_hit:KinematicCollision2D):
 func play_hit_sound():
 	hit_sound.play()
 
-func has_collided_with_player(object_hit:KinematicCollision2D):
+func has_collided_with_player(object_hit: KinematicCollision2D):
 	var object_hit_collider = object_hit.get_collider() as Node2D
 	return object_hit_collider.is_in_group("players")
 
-func get_distance_from_collider_center(object_hit:KinematicCollision2D)-> float:
+func get_distance_from_collider_center(object_hit: KinematicCollision2D) -> float:
 	var object_hit_collider = object_hit.get_collider() as Node2D
 	return position.y - object_hit_collider.position.y
 
-func calculate_bounce(object_hit:KinematicCollision2D):
-	direction= direction.bounce(object_hit.get_normal())
+func calculate_bounce(object_hit: KinematicCollision2D):
+	direction = direction.bounce(object_hit.get_normal())
 	
-	if(has_collided_with_player(object_hit)):
+	if (has_collided_with_player(object_hit)):
 		var distance_from_collider_center = get_distance_from_collider_center(object_hit)
-		direction+=Vector2(0, distance_from_collider_center * FAKE_CURVE_FACTOR)
+		direction += Vector2(0, distance_from_collider_center * FAKE_CURVE_FACTOR)
 	
 	direction = direction.normalized()
-	
